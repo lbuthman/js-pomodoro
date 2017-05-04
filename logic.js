6ecbacc840637b4c8b2da83810db4dd10f7f3559
lbuthman <lbuthman@gmail.com>
2017-05-03 19:25:00 -0500

    Add increment and decrement functionality

--- logic.js
+++ logic.js
@@ -1,6 +1,49 @@
-/**
- * Created by lbuthman on 5/2/17.
- */
 $(document).ready(function() {
 
+    $("button").click(function() {
+        var value;
+        var pomNum = "#pom-num";
+        var workNum = "#pom-num";
+        var breakNum = "#pom-num";
+        var type = $(this).attr("id");
+        switch (type) {
+            case "pom-plus":
+                value = parseInt($(pomNum).text()) + 1;
+                if (value <= 5) {
+                    $(pomNum).text(value);
+                }
+                break;
+            case "pom-minus":
+                value = parseInt($(pomNum).text()) - 1;
+                if (value >= 0) {
+                    $(pomNum).text(value);
+                }
+                break;
+            case "work-plus":
+                value = parseInt($(workNum).text()) + 5;
+                if (value <= 90) {
+                    $(workNum).text(value);
+                }
+                break;
+            case "work-minus":
+                value = parseInt($(workNum).text()) - 5;
+                if (value >= 5) {
+                    $(workNum).text(value);
+                }
+                break;
+            case "break-plus":
+                value = parseInt($(breakNum).text()) + 1;
+                if (value <= 20) {
+                    $(breakNum).text(value);
+                }
+                break;
+            case "break-minus":
+                value = parseInt($(breakNum).text()) - 1;
+                if (value >= 1) {
+                    $(breakNum).text(value);
+                }
+                break;
+            default:
+        }
+    });
 });
