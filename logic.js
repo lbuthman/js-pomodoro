20ac46e9387a3699c1cce3dc5c99ccfe2faee9a5
lbuthman <lbuthman@gmail.com>
2017-05-04 20:25:05 -0500

    Add timer functionality, start, pause and restart

--- logic.js
+++ logic.js
@@ -1,10 +1,55 @@
+//default values
+var defaultPoms = 3;
+var timerMinute = 25;
+var timerSeconds = 0;
+var startTime = timerMinute;
+var timerInterval;
+
+//selectors
+var timerSelector = "#timer";
+
 $(document).ready(function() {
-
-    var DEFAULT_POMS = 3;
-    renderPomodoro(DEFAULT_POMS);
-
+    renderPomodoro(defaultPoms);
 });
 
+/*handle start button click event*/
+$("#start").click(function() {
+    timerInterval = setInterval(function(){
+        if (timerMinute === 0 && timerSeconds === 0) {
+            clearInterval(timerInterval);
+            return;
+        }
+
+        if (timerSeconds === 0) {
+            timerSeconds = 59;
+            timerMinute -= 1;
+        } else {
+            timerSeconds -= 1;
+        }
+
+        if (timerSeconds < 10) {
+            $(timerSelector).text(timerMinute + ":" + "0" + timerSeconds);
+
+        } else {
+            $(timerSelector).text(timerMinute + ":" + timerSeconds);
+        }
+    }, 1000);
+});
+
+/*handle pause button click event*/
+$("#pause").click(function() {
+    clearInterval(timerInterval);
+});
+
+/*handle refresh button click event*/
+$("#restart").click(function() {
+    clearInterval(timerInterval);
+    timerMinute = startTime;
+    timerSeconds = 0;
+    $(timerSelector).text(timerMinute + ":" + "0" + timerSeconds);
+});
+
+/*handle pomodoro settings button clicks*/
 $("button").click(function() {
     var value;
     var pomNum = "#pom-num";
@@ -27,15 +72,19 @@ $("button").click(function() {
             }
             break;
         case "work-plus":
-            value = parseInt($(workNum).text()) + 5;
-            if (value <= 90) {
-                $(workNum).text(value);
+            startTime = parseInt($(workNum).text()) + 5;
+            timerMinute = startTime;
+            if (startTime <= 90) {
+                $(workNum).text(timerMinute);
+                $(timerSelector).text(timerMinute + ":00");
             }
             break;
         case "work-minus":
-            value = parseInt($(workNum).text()) - 5;
-            if (value >= 5) {
-                $(workNum).text(value);
+            startTime = parseInt($(workNum).text()) - 5;
+            timerMinute = startTime;
+            if (startTime >= 5) {
+                $(workNum).text(timerMinute);
+                $(timer).text(timerMinute + ":00");
             }
             break;
         case "break-plus":
@@ -55,13 +104,14 @@ $("button").click(function() {
 });
 
 function renderPomodoro(value) {
-    var pom = "http://www.myiconfinder.com/uploads/iconsets/871cc5a8a5f00c21047fd7342403b4d3-tomato.png";
+    var pomImage = "http://www.myiconfinder.com/uploads/iconsets/871cc5a8a5f00c21047fd7342403b4d3-tomato.png";
+    var crushedImage = "https://cdn1.iconfinder.com/data/icons/Hooligans_Icons/250/Tomato.png";
     var pomId = "#pom-holder";
 
     //reset div
     $(pomId).html("");
 
     for (var i=0; i<value; i++) {
-        $(pomId).append("<image class='pomodoro' id='pom-" + (value + 1) + "'" + "src='" + pom + "' alt='Pomodoro Tomato'>");
+        $(pomId).append("<image class='pomodoro' id='pom-" + (value + 1) + "'" + "src='" + pomImage + "' alt='Pomodoro Tomato'>");
     }
 }
