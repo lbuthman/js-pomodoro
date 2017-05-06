1767bca66214d3bdbe01f8c85a4b4af19e2cd82b
lbuthman <lbuthman@gmail.com>
2017-05-05 20:50:18 -0500

    Add rest interval functionality, and refactored code with helper functions and new variables

--- logic.js
+++ logic.js
@@ -1,74 +1,75 @@
 //default values
-var defaultPoms = 3;
-var timerMinute = 25;
+var timerMinute = 1;
 var timerSeconds = 0;
 var startTime = timerMinute;
 var timerInterval;
+var restMinutes = 1;
+var restSeconds = 0;
+var startRest = restMinutes;
+var restInterval;
+var RESTING = false;
+var totalPoms = 3;
+var crushedPoms = 0;
+var WHOLE_IMAGE = "http://www.myiconfinder.com/uploads/iconsets/871cc5a8a5f00c21047fd7342403b4d3-tomato.png";
+var CRUSHED_IMAGE = "https://cdn1.iconfinder.com/data/icons/Hooligans_Icons/250/Tomato.png";
+var ON_COLOR = "#E83F6F"; //Paradise Pink
+var REST_COLOR = "#1B998B"; //Jungle Green
+var RESET_COLOR = "#313E50"; //Charcoal
 
 //selectors
 var timerSelector = "#timer";
 
 $(document).ready(function() {
-    renderPomodoro(defaultPoms);
+    renderPomodoro(totalPoms);
 });
 
 /*handle start button click event*/
 $("#start").click(function() {
-    timerInterval = setInterval(function(){
-        if (timerMinute === 0 && timerSeconds === 0) {
-            clearInterval(timerInterval);
-            return;
-        }
+    if (RESTING) {
+        restTimer();
+    } else {
+        workTimer();
+    }
 
-        if (timerSeconds === 0) {
-            timerSeconds = 59;
-            timerMinute -= 1;
-        } else {
-            timerSeconds -= 1;
-        }
-
-        if (timerSeconds < 10) {
-            $(timerSelector).text(timerMinute + ":" + "0" + timerSeconds);
-
-        } else {
-            $(timerSelector).text(timerMinute + ":" + timerSeconds);
-        }
-    }, 1000);
 });
 
 /*handle pause button click event*/
 $("#pause").click(function() {
     clearInterval(timerInterval);
+    clearInterval(restInterval);
 });
 
 /*handle refresh button click event*/
 $("#restart").click(function() {
+    $("#timer").css("background-color", RESET_COLOR);
     clearInterval(timerInterval);
+    clearInterval(restInterval);
     timerMinute = startTime;
     timerSeconds = 0;
+    restMinutes = startRest;
+    restSeconds = 0;
     $(timerSelector).text(timerMinute + ":" + "0" + timerSeconds);
 });
 
 /*handle pomodoro settings button clicks*/
 $("button").click(function() {
-    var value;
     var pomNum = "#pom-num";
     var workNum = "#work-num";
     var breakNum = "#break-num";
     var type = $(this).attr("id");
     switch (type) {
         case "pom-plus":
-            value = parseInt($(pomNum).text()) + 1;
-            if (value <= 5) {
-                $(pomNum).text(value);
-                renderPomodoro(value);
+            totalPoms = parseInt($(pomNum).text()) + 1;
+            if (totalPoms <= 5) {
+                $(pomNum).text(totalPoms);
+                renderPomodoro(totalPoms);
             }
             break;
         case "pom-minus":
-            value = parseInt($(pomNum).text()) - 1;
-            if (value >= 1) {
-                $(pomNum).text(value);
-                renderPomodoro(value);
+            totalPoms = parseInt($(pomNum).text()) - 1;
+            if (totalPoms >= 1) {
+                $(pomNum).text(totalPoms);
+                renderPomodoro(totalPoms);
             }
             break;
         case "work-plus":
@@ -88,30 +89,106 @@ $("button").click(function() {
             }
             break;
         case "break-plus":
-            value = parseInt($(breakNum).text()) + 1;
-            if (value <= 20) {
-                $(breakNum).text(value);
+            startRest = parseInt($(breakNum).text()) + 1;
+            restMinutes = startRest;
+            if (restMinutes <= 20) {
+                $(breakNum).text(restMinutes);
             }
             break;
         case "break-minus":
-            value = parseInt($(breakNum).text()) - 1;
-            if (value >= 1) {
-                $(breakNum).text(value);
+            startRest = parseInt($(breakNum).text()) - 1;
+            restMinutes = startRest;
+            if (restMinutes >= 1) {
+                $(breakNum).text(restMinutes);
             }
             break;
         default:
     }
 });
 
+function workTimer() {
+    $("#timer").css("background-color", ON_COLOR);
+    updateWorkTimerText();
+
+    timerInterval = setInterval(function(){
+        if (timerMinute === 0 && timerSeconds === 0) {
+            clearInterval(timerInterval);
+            crushedPoms += 1;
+            renderPomodoro(totalPoms);
+            RESTING = true;
+            restMinutes = startRest;
+            restTimer();
+        } else {
+            if (timerSeconds === 0) {
+                timerSeconds = 59;
+                timerMinute -= 1;
+            } else {
+                timerSeconds -= 1;
+            }
+
+            updateWorkTimerText();
+        }
+
+    }, 1000);
+}
+
+function updateWorkTimerText() {
+    if (timerSeconds < 10) {
+        $(timerSelector).text(timerMinute + ":" + "0" + timerSeconds);
+    } else {
+        $(timerSelector).text(timerMinute + ":" + timerSeconds);
+    }
+}
+
+function restTimer() {
+
+    $("#timer").css("background-color", REST_COLOR);
+    updateRestTimerText();
+
+    restInterval = setInterval(function(){
+        if (restMinutes === 0 && restSeconds === 0) {
+            clearInterval(restInterval);
+            RESTING = false;
+            timerMinute = startTime;
+            workTimer();
+        } else {
+            if (restSeconds === 0) {
+                restSeconds = 59;
+                restMinutes -= 1;
+            } else {
+                restSeconds -= 1;
+            }
+
+            updateRestTimerText();
+        }
+
+    }, 1000);
+}
+
+function updateRestTimerText() {
+    if (restSeconds < 10) {
+        $(timerSelector).text(restMinutes + ":" + "0" + restSeconds);
+    } else {
+        $(timerSelector).text(restMinutes + ":" + restSeconds);
+    }
+}
+
 function renderPomodoro(value) {
-    var pomImage = "http://www.myiconfinder.com/uploads/iconsets/871cc5a8a5f00c21047fd7342403b4d3-tomato.png";
-    var crushedImage = "https://cdn1.iconfinder.com/data/icons/Hooligans_Icons/250/Tomato.png";
+
     var pomId = "#pom-holder";
+    var pomImage;
+    var p = crushedPoms;
 
     //reset div
     $(pomId).html("");
 
     for (var i=0; i<value; i++) {
+        if (p > 0) {
+            pomImage = CRUSHED_IMAGE;
+            p -= 1;
+        } else {
+            pomImage = WHOLE_IMAGE;
+        }
         $(pomId).append("<image class='pomodoro' id='pom-" + (value + 1) + "'" + "src='" + pomImage + "' alt='Pomodoro Tomato'>");
     }
 }
